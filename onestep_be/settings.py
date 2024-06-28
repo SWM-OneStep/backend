999e791537d10cb6e2df17ea288dc35ac3a6b260
earthyoung <studydatawithme@gmail.com>
2024-06-28 19:40:06 +0900

    fix: corsheaders 설정 추가

--- onestep_be/settings.py
+++ onestep_be/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["*"]
 
 # Application definition
 
@@ -42,9 +44,11 @@ INSTALLED_APPS = [
     "accounts",
     "drf_yasg",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
