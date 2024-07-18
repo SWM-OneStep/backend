f56e29a22e2035a7905d80a5da598a9c95e6b574
earthyoung <studydatawithme@gmail.com>
2024-07-18 15:36:23 +0900

    feat: CustomRefreshToken 모델 만들기

--- accounts/views.py
+++ accounts/views.py
@@ -3,17 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from accounts.models import *
 from accounts.serializers import *
-import jwt
 import os
 from dotenv import load_dotenv
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from google.auth.transport import requests
 from google.oauth2 import id_token
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from accounts.serializers import *
+from accounts.tokens import CustomRefreshToken
 
 User = get_user_model()
 
@@ -43,14 +44,16 @@ class GoogleLogin(APIView):
             )
             if "accounts.google.com" in idinfo["iss"]:
                 email = idinfo["email"]
-                name = idinfo.get("name", "")
                 user, _ = User.objects.get_or_create(username=email, password="")
-                refresh = RefreshToken.for_user(user)
+                device_token = request.data.get("device_token")
+                if not device_token:
+                    raise Exception("device token is required")
+                refresh = CustomRefreshToken.for_user(user, device_token)
                 return Response(
                     {
                         "refresh": str(refresh),
                         "access": str(refresh.access_token),
                     }
                 )
-        except ValueError as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": str(e)})
