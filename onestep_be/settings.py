10cf04ea41ae7867ef194c91bd29edba30e782a5
earthyoung <studydatawithme@gmail.com>
2024-07-18 13:06:19 +0900

    feat: authentication class 추가

--- onestep_be/settings.py
+++ onestep_be/settings.py
@@ -66,7 +66,8 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "accounts.authentication.CustomJWTAuthentication",
     )
 }
 
