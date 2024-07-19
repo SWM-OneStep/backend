81082fd3a760aa809d05515a404b514703bb9050
earthyoung <studydatawithme@gmail.com>
2024-07-19 13:41:51 +0900

    fix: views, tests 수정

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,16 +1,21 @@
 from django.test import TestCase, override_settings
-from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient, force_authenticate
 from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from unittest.mock import patch
 from rest_framework.exceptions import PermissionDenied
+from accounts.views import UserRetrieveView
 import pytest
 import logging
 
 
+
 client = APIClient()
 User = get_user_model()
+factory = APIRequestFactory()
+view = UserRetrieveView.as_view()
 
 
 @pytest.fixture
@@ -23,12 +28,12 @@ def create_user(db):
     return user
 
 
-@override_settings(REST_FRAMEWORK={'DEFAULT_PERMISSION_CLASSES': []})
 @pytest.mark.django_db
 def test_user_info(create_user):
     with patch('accounts.views.UserRetrieveView.permission_classes', new=[]):
-        url = reverse('user', args=[1])
-        response = client.get(url)
+        request = factory.get(reverse('user'))
+        force_authenticate(request, user=create_user)
+        response = view(request)
         assert response.status_code == 200
         assert response.data == {
             'id': 1,
@@ -40,6 +45,6 @@ def test_user_info(create_user):
 
 @pytest.mark.django_db
 def test_user_info_without_permission(create_user):
-    url = reverse('user', args=[1])
-    client.get(url)
-    assert pytest.raises(PermissionDenied)
+    with pytest.raises(User.DoesNotExist):
+        request = factory.get(reverse('user'))
+        view(request)
