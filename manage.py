cdf1cd3605dba548dd7ad32894e7dcf9e94622f9
earthyoung <studydatawithme@gmail.com>
2024-08-12 13:45:35 +0900

    fix: DJANGO_SETTINGS_MODULE 기본값 settings.dev -> settings로 변경

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onestep_be.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onestep_be.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
