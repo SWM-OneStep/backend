6a3efaf5ee4ab15b3937e002cd490010a7ae9347
earthyoung <studydatawithme@gmail.com>
2024-08-24 21:21:14 +0900

    fix: locustfile test URL 변경

--- locustfile.py
+++ locustfile.py
@@ -3,22 +3,11 @@ from django.conf import settings
 import os
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onestep_be.setting.test")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onestep_be.setting.dev")
 
 class TestTodos(HttpUser):
 
-    def on_start(self) -> None:
-        test_db_info = settings.DATABASES.get("test")
-        self.db_host = test_db_info.get("HOST")
-        self.db_name = test_db_info.get("NAME")
-        self.db_user = test_db_info.get("USER")
-        self.db_password = test_db_info.get("PASSWORD")
-
     @task
-    def retrieve_todo(self) -> None:
-        self.client.get("/todos/todo?user_id=1")
-
-    @task
-    def create_todo(self) -> None:
-        self.client.post("/todos/todo?user_id=1")
+    def get_android_client_id(self) -> None:
+        self.client.get("/auth/android")
 
