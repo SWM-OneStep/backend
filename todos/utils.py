36865efa3d4f6ed80d5f02ac19155fcbf7475071
alpakaka000808@gmail.com <alpakaka000808@gmail.com>
2024-09-07 16:33:47 +0900

    fix : delete util sentry_data

--- todos/utils.py
+++ todos/utils.py
@@ -1,5 +1,3 @@
-import sentry_sdk
-
 from todos.lexorank import LexoRank
 
 
@@ -24,19 +22,3 @@ def validate_lexo_order(prev, next, updated):
         ):
             return False
     return True
-
-
-def sentry_data(method, url, status_code, headers):
-    span = sentry_sdk.start_span(
-        op="http.request",
-        description="%s %s" % (method, url),
-    )
-
-    span.set_data("http.reqeust.method", method)
-
-    span.set_data("http.request.url", url)
-
-    span.set_data("http.response.status_code", status_code)
-    span.set_data("http.response_content_length", headers["content-length"])
-
-    span.finish()
