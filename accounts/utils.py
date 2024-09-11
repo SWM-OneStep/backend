d179936dd010dcb6c1e33afcf73ac3930b7f7aa5
alpakaka000808@gmail.com <alpakaka000808@gmail.com>
2024-09-11 18:14:52 +0900

    feat: welcome mail

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,4 +1,5 @@
 import resend
+from django.template.loader import render_to_string
 
 
 def send_email(send_email, subject, message):
@@ -11,3 +12,10 @@ def send_email(send_email, subject, message):
 
     email = resend.Emails.send(params)
     return email
+
+
+def welcome_email(user_name):
+    html_message = render_to_string(
+        "welcome_email.html", {"user_name": user_name}
+    )
+    return html_message
